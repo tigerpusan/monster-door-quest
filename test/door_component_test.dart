91f9927280f2945047fbd55e49044f32302bf6ff
tigerpusan <tigerpusan74@gmail.com>
2026-08-27 16:49:37 +0900

    Update V0.7.0 fast door timing test

--- test/door_component_test.dart
+++ test/door_component_test.dart
@@ -3,12 +3,16 @@ import 'package:monster_door_quest/game/components/door_component.dart';
 import 'package:monster_door_quest/game/core/game_rules.dart';
 
 void main() {
-  test('door reacts immediately before open animation completes', () {
+  test('door reacts immediately with V0.7 fast open timing', () {
     final door = DoorComponent(side: DoorSide.left, onSelected: (_) {});
+
     door.pressDown();
     expect(door.isPressed, isTrue);
+
     door.open(correct: true);
     expect(door.isOpening, isTrue);
-    expect(door.openDuration, inInclusiveRange(.25, .35));
+
+    // V0.7.0 intentionally shortened the door animation for rapid sequential input.
+    expect(door.openDuration, inInclusiveRange(.12, .20));
   });
 }
